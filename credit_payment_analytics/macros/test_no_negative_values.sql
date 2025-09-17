641e282e9c70ad34b7705d31214f227e62ac4d0f
Rachel Berger <bergerache@gmail.com>
2025-09-17 22:07:59 +0100

    update macro name to test_no_negative_values

--- credit_payment_analytics/macros/test_no_negative_values.sql
+++ credit_payment_analytics/macros/test_no_negative_values.sql
@@ -1,6 +1,6 @@
 -- This macro tests that all values in the specified column are positive (greater than or equal to zero).
 
-{% macro test_positive_values(model, column_name) %}
+{% macro test_no_negative_values(model, column_name) %}
     SELECT *
     FROM {{ model }}
     WHERE {{ column_name }} < 0
